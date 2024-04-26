7d68d002a6a28e9e28a72dba4fb1716f9aa42e2e
Max Andersen <andersmx12@gmail.com>
2024-04-25 19:49:26 -0600

    Worked on Prep 1 and 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Enter your first name: ");
+        string first = Console.ReadLine();
+        Console.WriteLine("Enter your last name: ");
+        string second = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {second}, {first} {second}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+
+
+        if (number >= 90){
+            // Console.WriteLine("You have an A");
+            letter = "A";
+        }
+        else if (number >= 80 && number < 90){
+            //Console.WriteLine("You have a B");
+            letter = "B";
+        }
+        else if (number >= 70 && number < 80){
+            //Console.WriteLine("You have a C");
+            letter = "C";
+        }
+        else if (number >= 60 && number < 70){
+            //Console.WriteLine("You have a D");
+            letter = "D";
+        }
+        else if (number < 60){
+            Console.WriteLine("You are failing.");
+        }
+        else {
+            Console.WriteLine("That value is our of range.");
+        }
+
+        if (number >= 70){
+            Console.WriteLine($"You have a(n) {letter}! You are passing!");
+        }
+        else {
+            Console.WriteLine("You are failing");
+        }
     }
 }
