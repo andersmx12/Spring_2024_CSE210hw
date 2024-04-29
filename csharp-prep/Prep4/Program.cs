f7c835755b9f2fbd6bf27ab50e08bdd9ce9bfa86
Max Andersen <andersmx12@gmail.com>
2024-04-29 12:42:27 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int sum_num = 0; 
+        int num_num = 0;
+        // Loop
+        while (userNum != 0){
+        Console.Write("Enter a number: ");
+        string user_num = Console.ReadLine();
+        int userNum = int.Parse(user_num);
+
+        sum_num += userNum;
+        num_num += 1;
+        numbers.Add(userNum);
+    }
     }
 }
