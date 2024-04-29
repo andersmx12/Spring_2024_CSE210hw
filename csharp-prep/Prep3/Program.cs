1b2dd3eb9ea29800a4edd09a4dde8371269518c5
Max Andersen <andersmx12@gmail.com>
2024-04-29 12:01:19 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool correct = false;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        // Console.WriteLine("What is the magic number?");
+        // string magicNum = Console.ReadLine();
+        // int number = int.Parse(magicNum);
+
+while (correct == false){
+
+        Console.WriteLine("What is your guess?");
+        string guess = Console.ReadLine();
+        int test = int.Parse(guess);
+
+        
+
+        if (test < number){
+            Console.WriteLine("Higher");
+            Console.WriteLine(" ");
+            //correct = false;
+        }
+
+        else if (test > number){
+            Console.WriteLine("Lower");
+            Console.WriteLine(" ");
+            //correct = false;
+        }
+
+        else{
+            Console.WriteLine("Correct!");
+            correct = true;
+        }
+        }
     }
 }
